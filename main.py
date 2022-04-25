8639e6ed4ec0d10a767c7a03e97a63c2cbd43c03
ivan <ivchiga@gmail.com>
2022-04-25 13:51:38 +0100

    se usan recomendaciones mejora código

--- main.py
+++ main.py
@@ -1,46 +1,41 @@
-#!/usr/bin/python3
-
 import tkinter as tk
 from mostrar import *
 
-class Main:
-    
-    def __init__(self):
-        self.__iniciar()
-    
-    def __iniciar(self):
-        self.main_w = tk.Tk()
-        self.main_w.title("Gestión de recetas")
-        self.main_w.minsize(340,350)
-        self.main_w.resizable(width=False, height=False)
+class MainApplication():
+    def __init__(self, parent):
+        self.parent = parent
 
-        self.lbl1 = tk.Label(self.main_w, text="Gestión de recetas", font=("Arial Bold", 20))
+        self.parent.title("Gestión de recetas")
+        self.parent.minsize(340,350)
+        self.parent.resizable(width=False, height=False)
+
+        self.lbl1 = tk.Label(self.parent, text="Gestión de recetas", font=("Arial Bold", 20))
         self.lbl1.grid(column=0, row=0, padx=25, pady=5)
         
-        self.btn_0 = tk.Button(self.main_w, text="Mostrar recetas", font=("Arial Bold", 12))
+        self.btn_0 = tk.Button(self.parent, text="Mostrar recetas", font=("Arial Bold", 12))
         self.btn_0.grid(column=0, row=2, padx=25, pady=5, sticky='ew')
 
-        self.btn_1 = tk.Button(self.main_w, text="Mostrar receta", font=("Arial Bold", 12), command = self.__btn_1_click)
+        self.btn_1 = tk.Button(self.parent, text="Mostrar receta", font=("Arial Bold", 12), command = self.__btn_1_click)
         self.btn_1.grid(column=0, row=3, padx=25, pady=5, sticky='ew')
         
-        self.btn_2 = tk.Button(self.main_w, text="Mostrar recetas con ingredientes", font=("Arial Bold", 12))
+        self.btn_2 = tk.Button(self.parent, text="Mostrar recetas con ingredientes", font=("Arial Bold", 12))
         self.btn_2.grid(column=0, row=4, padx=25, pady=5, sticky='ew')
         
-        self.btn_3 = tk.Button(self.main_w, text="Nueva receta", font=("Arial Bold", 12))
+        self.btn_3 = tk.Button(self.parent, text="Nueva receta", font=("Arial Bold", 12))
         self.btn_3.grid(column=0, row=5, padx=25, pady=5, sticky='ew')
         
-        self.btn_3 = tk.Button(self.main_w, text="Añadir ingredientes", font=("Arial Bold", 12))
+        self.btn_3 = tk.Button(self.parent, text="Añadir ingredientes", font=("Arial Bold", 12))
         self.btn_3.grid(column=0, row=6, padx=25, pady=5, sticky='ew')
         
-        self.btn_4 = tk.Button(self.main_w, text="Borrar receta", font=("Arial Bold", 12))
+        self.btn_4 = tk.Button(self.parent, text="Borrar receta", font=("Arial Bold", 12))
         self.btn_4.grid(column=0, row=7, padx=25, pady=5, sticky='ew')
 
-        self.main_w.mainloop()
-        
-    def __btn_1_click(self):
-        mostrar = MostrarReceta()
-    
 
+    def __btn_1_click(self):
+        op_mostrar_r = tk.Toplevel()
+        MostrarReceta(op_mostrar_r)
         
 if __name__ == '__main__':
-    main = Main()
+    root = tk.Tk()
+    MainApplication(root)
+    root.mainloop()
--- mostrar.py
+++ mostrar.py
@@ -3,48 +3,45 @@ from tkinter import messagebox
 
 from receta import *
 
-class MostrarReceta:
-    def __init__(self):
-        self._iniciar()
+class MostrarReceta():
+    def __init__(self, parent):
         
-    def _iniciar(self):
-        # Se crea una ventana hija. No puede haber dos instancias de Tk() en ejecución
-        self.mostrar_w = tk.Toplevel()  
+        self.parent = parent  
         
-        self.mostrar_w.title("Mostrar Receta")
-        self.mostrar_w.minsize(400, 150)
-        self.mostrar_w.resizable(width=False, height=False)
+        self.parent.title("Mostrar Receta")
+        self.parent.minsize(400, 150)
+        self.parent.resizable(width=False, height=False)
         
-        self.lbl = tk.Label(self.mostrar_w, text="Mostrar Receta", font=("Arial Bold", 20))
+        self.lbl = tk.Label(self.parent, text="Mostrar Receta", font=("Arial Bold", 20))
         self.lbl.grid(column=0, row=0, columnspan=2, padx=5, pady=5)
 
         
-        self.lbl_n = tk.Label(self.mostrar_w, text="Nombre de la receta:", font=("Arial", 12))    
+        self.lbl_n = tk.Label(self.parent, text="Nombre de la receta:", font=("Arial", 12))    
         self.lbl_n.grid(column=0, row=1, padx=10, pady=5, sticky = "w")
         
-        self.txt_n = tk.Entry(self.mostrar_w, width=25)
+        self.txt_n = tk.Entry(self.parent, width=25)
         self.txt_n.grid(column=1, row=1, padx=5, pady=5)
         self.txt_n.focus_set()           # Asignamos foco a este campo
         
-        self.btn = tk.Button(self.mostrar_w, text="Enviar", font=("Arial", 12), command = self._form1_enviar)
+        self.btn = tk.Button(self.parent, text="Enviar", font=("Arial", 12), command = self._form1_enviar)
         self.btn.grid(column=1, row=3, sticky='ew', padx=5, pady=5)
         
         # Al pulsar Enter ejecutar método
-        self.mostrar_w.bind('<Return>', self._form1_enviar)
+        self.parent.bind('<Return>', self._form1_enviar)
         
         
     def _form1_enviar(self, event = ""):        # bind pasa evento como parámetro
         nombre = self.txt_n.get()
         if nombre == "":
             # Ponemos atributo parent para que el mensaje se muestre encima de la ventana padre
-            messagebox.showerror("error", "No se ha introducido ningún valor", parent = self.mostrar_w)
+            messagebox.showerror("error", "No se ha introducido ningún valor", parent = self.parent)
             
         else:
             receta = Receta()
             text_receta = receta.mostrar_receta(nombre)   # Obtenemos información de la receta
             if not text_receta:
-                messagebox.showerror("error", "La receta no existe", parent = self.mostrar_w)
+                messagebox.showerror("error", "La receta no existe", parent = self.parent)
                 
             else:
-                self.mostrar_w.destroy()
+                self.parent.destroy()
                 messagebox.showinfo(nombre, text_receta)
