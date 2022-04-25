ee04c80daff0fff3d5f21964cd29262644d4ae70
ivan <ivchiga@gmail.com>
2022-04-25 12:31:00 +0100

    se añade toplevel y quita mainloop en opciones

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 
-from tkinter import *
-from tkinter import messagebox
+import tkinter as tk
 from mostrar import *
 
 class Main:
@@ -10,30 +9,30 @@ class Main:
         self.__iniciar()
     
     def __iniciar(self):
-        self.main_w = Tk()
+        self.main_w = tk.Tk()
         self.main_w.title("Gestión de recetas")
-        self.main_w.minsize(340,320)
+        self.main_w.minsize(340,350)
         self.main_w.resizable(width=False, height=False)
 
-        self.lbl1 = Label(self.main_w, text="Gestión de recetas", font=("Arial Bold", 20))
+        self.lbl1 = tk.Label(self.main_w, text="Gestión de recetas", font=("Arial Bold", 20))
         self.lbl1.grid(column=0, row=0, padx=25, pady=5)
         
-        self.btn_0 = Button(self.main_w, text="Mostrar recetas", font=("Arial Bold", 12))
+        self.btn_0 = tk.Button(self.main_w, text="Mostrar recetas", font=("Arial Bold", 12))
         self.btn_0.grid(column=0, row=2, padx=25, pady=5, sticky='ew')
 
-        self.btn_1 = Button(self.main_w, text="Mostrar receta", font=("Arial Bold", 12), command = self.__btn_1_click)
+        self.btn_1 = tk.Button(self.main_w, text="Mostrar receta", font=("Arial Bold", 12), command = self.__btn_1_click)
         self.btn_1.grid(column=0, row=3, padx=25, pady=5, sticky='ew')
         
-        self.btn_2 = Button(self.main_w, text="Mostrar recetas con ingredientes", font=("Arial Bold", 12))
+        self.btn_2 = tk.Button(self.main_w, text="Mostrar recetas con ingredientes", font=("Arial Bold", 12))
         self.btn_2.grid(column=0, row=4, padx=25, pady=5, sticky='ew')
         
-        self.btn_3 = Button(self.main_w, text="Nueva receta", font=("Arial Bold", 12))
+        self.btn_3 = tk.Button(self.main_w, text="Nueva receta", font=("Arial Bold", 12))
         self.btn_3.grid(column=0, row=5, padx=25, pady=5, sticky='ew')
         
-        self.btn_3 = Button(self.main_w, text="Añadir ingredientes", font=("Arial Bold", 12))
+        self.btn_3 = tk.Button(self.main_w, text="Añadir ingredientes", font=("Arial Bold", 12))
         self.btn_3.grid(column=0, row=6, padx=25, pady=5, sticky='ew')
         
-        self.btn_4 = Button(self.main_w, text="Borrar receta", font=("Arial Bold", 12))
+        self.btn_4 = tk.Button(self.main_w, text="Borrar receta", font=("Arial Bold", 12))
         self.btn_4.grid(column=0, row=7, padx=25, pady=5, sticky='ew')
 
         self.main_w.mainloop()
--- mostrar.py
+++ mostrar.py
@@ -1,4 +1,4 @@
-from tkinter import *
+import tkinter as tk
 from tkinter import messagebox
 
 from receta import *
@@ -8,29 +8,28 @@ class MostrarReceta:
         self._iniciar()
         
     def _iniciar(self):
-        self.mostrar_w = Tk()
+        self.mostrar_w = tk.Toplevel()
         self.mostrar_w.title("Mostrar Receta")
         self.mostrar_w.minsize(400, 150)
         self.mostrar_w.resizable(width=False, height=False)
         
-        self.lbl = Label(self.mostrar_w, text="Mostrar Receta", font=("Arial Bold", 20))
+        self.lbl = tk.Label(self.mostrar_w, text="Mostrar Receta", font=("Arial Bold", 20))
         self.lbl.grid(column=0, row=0, columnspan=2, padx=5, pady=5)
 
         
-        self.lbl_n = Label(self.mostrar_w, text="Nombre de la receta:", font=("Arial", 12))    
+        self.lbl_n = tk.Label(self.mostrar_w, text="Nombre de la receta:", font=("Arial", 12))    
         self.lbl_n.grid(column=0, row=1, padx=10, pady=5, sticky = "w")
         
-        self.txt_n = Entry(self.mostrar_w, width=25)
+        self.txt_n = tk.Entry(self.mostrar_w, width=25)
         self.txt_n.grid(column=1, row=1, padx=5, pady=5)
         self.txt_n.focus_set()           # Asignamos foco a este campo
         
-        self.btn = Button(self.mostrar_w, text="Enviar", font=("Arial", 12), command = self._form1_enviar)
+        self.btn = tk.Button(self.mostrar_w, text="Enviar", font=("Arial", 12), command = self._form1_enviar)
         self.btn.grid(column=1, row=3, sticky='ew', padx=5, pady=5)
         
         # Al pulsar Enter ejecutar método
         self.mostrar_w.bind('<Return>', self._form1_enviar)
         
-        self.mostrar_w.mainloop()
         
     def _form1_enviar(self, event = ""):        # bind pasa evento como parámetro
         nombre = self.txt_n.get()
